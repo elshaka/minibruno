105d8f82e823c29f8e38edac9afa095ab0ab2a78
Eleazar Meza <meza.eleazar@gmail.com>
2014-08-30 12:38:19 -0430

    Forced binary mode when saving IMGKit jpeg to disk IMGKit.to_file uses ASCII-8BIT which generates corrupted jpeg files on Windows

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,18 +2,21 @@ module ReportsHelper
   @@ac = ApplicationController.new
 
   def render_flot(view, data, width, height)
-    IMGKit.new(
-      @@ac.render_to_string(
-        "reports/#{view}.flot",
-        layout: 'flot',
-        locals: {
-          data: data,
-          width: width,
-          height: height
-        }
-      ),
-      crop_w: width,
-      crop_h: height
-    ).to_file("tmp/#{view}.jpg").path
+    File.open("tmp/#{view}.jpg", 'wb') do |file|
+      file << IMGKit.new(
+        @@ac.render_to_string(
+          "reports/#{view}.flot",
+          layout: 'flot',
+          locals: {
+            data: data,
+            width: width,
+            height: height
+          }
+        ),
+        crop_w: width,
+        crop_h: height
+      ).to_jpg
+      file.path
+    end
   end
 end
